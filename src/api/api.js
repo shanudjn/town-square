c61f3f1759168aec65a1b72cb8d2a13c5f26e632
Md Shahazad Uddin <shanudjn@gmail.com>
2021-06-16 15:33:28 +0530

    updated base url

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 // export const baseUrl = "http://localhost:8080/api/"
-export const baseUrl = "https://town-square-backend.herokuapp.com/"
+export const baseUrl = "https://town-square-backend.herokuapp.com/api/"
 
 
 
